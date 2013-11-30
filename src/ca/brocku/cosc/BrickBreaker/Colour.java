78c88906606c1051a1302d8fa3285b8605a7c181
lapp14 <d.lapp14@gmail.com>
2013-11-30 00:18:21 -0500

    collision

--- src/ca/brocku/cosc/BrickBreaker/Colour.java
+++ src/ca/brocku/cosc/BrickBreaker/Colour.java
@@ -3,51 +3,21 @@ package ca.brocku.cosc.BrickBreaker;
 /**
  * Created by dl08ti on 18/11/13.
  */
-public class Colour {
-    private int r, g, b;
-
-    public Colour(int r, int g, int b) {
-	this.setR(r);
-	this.setG(g);
-	this.setB(b);
-    }
-
-    void setR(int r) {
-	if (r < 0)
-	    this.r = 0;
-	else if (r > 255)
-	    this.r = 255;
-	else
-	    this.r = r;
-    }
-
-    void setG(int g) {
-	if (g < 0)
-	    this.g = 0;
-	else if (g > 255)
-	    this.g = 255;
-	else
-	    this.g = g;
-    }
-
-    void setB(int b) {
-	if (b < 0)
-	    this.b = 0;
-	else if (b > 255)
-	    this.b = 255;
-	else
-	    this.b = b;
-    }
-
-    int getR() {
-	return r;
-    }
-
-    int getG() {
-	return g;
-    }
-
-    int getB() {
-	return b;
-    }
+public class Colour 
+{
+	/**
+	 * Colors come in 3 intensities.
+	 * 
+	 * COLOR[0] = bright
+	 * COLOR[1] = medium
+	 * COLOR[2] = dark
+	 */
+	
+    public static final int[] RED = {0xffff0000, 0xffc60c12, 0xffa91415};
+    
+    public static final int[] ORANGE = {0xffffa63d, 0xfff9a239, 0xffda7c0b};
+    
+    public static final int[] GREEN1 = {0xffcdf750, 0xffabd822, 0xff7a9721};
+    
+    public static final int[] GREEN2 = {0xff8efc4a, 0xff6ae61e, 0xff4e9721};
 }
